364a56736a22f6e2fa9e9aff972c299d1cc4e13f
Niang <141337131+Ndniang@users.noreply.github.com>
2025-04-09 15:43:04 +0000

    Update ndeye.py

--- ndeye.py
+++ ndeye.py
@@ -1,10 +1,10 @@
-#import joblib
+import joblib
 import streamlit as st
 import numpy as np
 
 # Charger le modèle
 model_path = "expresso.pkl"
-#model = joblib.load(model_path)
+model = joblib.load(model_path)
 
 
 # Entrées utilisateur
